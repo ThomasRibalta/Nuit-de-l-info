f6a43c0537286aaea369ea56afd0364241bcb4a3
Thomas <thomasribalta@gmail.com>
2024-12-06 00:01:32 +0100

    add Cgu + gitignore

--- nuit-de-linfo-front/src/App.jsx
+++ nuit-de-linfo-front/src/App.jsx
@@ -6,6 +6,7 @@ import Quizz from "./Quizz/Quizz.jsx";
 import LoginForm from "./pages/Login.jsx";
 import RegisterForm from "./pages/Register.jsx";
 import MentionLegale from "./pages/Mention.jsx";
+import Cgu from "./pages/Cgu.jsx";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/login" element={<LoginForm />}></Route>
         <Route path="/register" element={<RegisterForm />}></Route>
         <Route path="/mention" element={<MentionLegale />}></Route>
+        <Route path="/cgu" element={<Cgu />}></Route>
         <Route path="*" element={<h1>404 Not Found</h1>}></Route>
       </Routes>
     </>
---
+++ nuit-de-linfo-front/src/pages/Cgu.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "../coconfor.css"; // Assurez-vous que ce fichier est disponible
+
+const Cgu = () => {
+  return (
+    <div className="container">
+      <h1>Conditions Générales d'Utilisation (CGU)</h1>
+      <p>Dernière mise à jour : 5 décembre 2024</p>
+      <p>
+        Les présentes Conditions Générales d'Utilisation (CGU) régissent l'accès
+        et l'utilisation du site web (ci-après le "Site"). En accédant et en
+        utilisant ce Site, vous acceptez les présentes CGU. Si vous n'acceptez
+        pas ces conditions, veuillez ne pas utiliser ce Site.
+      </p>
+      <h2>1. Présentation du Site</h2>
+      <p>
+        Le Site est une plateforme de présentation , destinée à fournir des
+        informations sur et à promouvoir ses activités. Il ne propose aucune
+        fonctionnalité interactive, sauf les informations statiques et les liens
+        vers des pages externes ou des moyens de contact.
+      </p>
+      <h2>2. Accès au Site</h2>
+      <p>
+        L'accès au Site est libre et gratuit pour tout utilisateur disposant
+        d'un accès à Internet. Tous les frais relatifs à l'accès au Site, que ce
+        soit les frais matériels, logiciels ou d'accès à Internet, restent à la
+        charge de l'utilisateur.
+      </p>
+      <p>
+        Le Site est accessible 24 heures sur 24, 7 jours sur 7, sous réserve des
+        éventuelles pannes ou des interventions de maintenance nécessaires au
+        bon fonctionnement du Site.
+      </p>
+      <h2>3. Propriété intellectuelle</h2>
+      <p>
+        Tous les éléments présents sur le Site, incluant mais ne se limitant pas
+        aux textes, images, graphismes, logos, vidéos, marques, sons et
+        logiciels, sont la propriété exclusive de l'équipe Coconfor ou de ses
+        partenaires, et sont protégés par les lois en vigueur relatives à la
+        propriété intellectuelle. Toute reproduction ou utilisation, totale ou
+        partielle, de ces éléments, sans autorisation préalable, est strictement
+        interdite.
+      </p>
+      <h2>4. Responsabilité</h2>
+      <p>
+        L'équipe Coconfor ne peut être tenu responsable des erreurs ou omissions
+        présentes sur le Site. Le contenu du Site est fourni "en l'état" et peut
+        être modifié sans préavis.
+      </p>
+      <p>
+        L'équipe Coconfor décline également toute responsabilité pour les
+        éventuels dommages directs ou indirects pouvant résulter de
+        l'utilisation du Site ou de l'incapacité d'y accéde
+      </p>
+      <h2>5. Liens hypertextes</h2>
+      <p>
+        Le Site peut contenir des liens vers d'autres sites Internet. Ces liens
+        sont fournis pour votre confort et votre information. L'équipe Coconfor
+        n'assume aucune responsabilité quant au contenu de ces sites externes.
+      </p>
+      <h2>6. Données personnelles</h2>
+      <p>
+        Le Site ne collecte pas de données personnelles de manière active, à
+        moins que cela ne soit nécessaire dans le cadre de l'envoi
+        d'informations ou de demandes de contact. Pour plus d'informations,
+        veuillez consulter notre Politique de Confidentialité.
+      </p>
+      <h2>7. Modification des CGU</h2>
+      <p>
+        L'équipe Coconfor se réserve le droit de modifier les présentes
+        Conditions Générales d'Utilisation à tout moment, sans préavis. Les
+        utilisateurs sont invités à consulter régulièrement cette page pour
+        prendre connaissance des éventuelles modifications.
+      </p>
+      <h2>8. Loi applicable et juridiction compétente</h2>
+      <p>
+        Les présentes Conditions Générales d'Utilisation sont régies par la loi
+        française. En cas de litige, le tribunal compétent sera celui de France.
+      </p>
+    </div>
+  );
+};
+
+export default Cgu;

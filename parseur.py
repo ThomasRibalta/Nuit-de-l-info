9f9592e1b0f1433498e80656c7c79df5217c83b4
Jordan <guaglio.jordan@gmail.com>
2024-12-05 23:02:39 +0100

    generate parseur

--- parseur.py
+++ parseur.py
@@ -1,36 +1,32 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:3030")
-
-db = client["nuitInfo"]
-
-collection = db["Quizz"]
-
-def read3line() : 
-	i = 0;
+def read3line (fd, collection) : 
+	i = 0 
 	j = 0
 	h = 0
 	ligne = [] 
-
-	fd = open("question.txt", mode = 'r', encodinf = 'utf-8')
 	while (i < 3) :
-		ligne[i] = fd.readline()
-		if (ligne[i] == None):
+		ligne.append(fd.readline())
+		if (ligne[i] == ''):
 			return (False)
+		i += 1
 	i = 0
 	if (ligne[i] == '-') :
-		ligne[i] = ligne[i + 2]
+		ligne[i] = ligne[i][2 : ]
 		i = i + 1
-		while (j < 2):
-			while (ligne[i][h] != ':'):
-				h+=1
-			ligne[i] = ligne[i][h]
-
-		
-	document = {"nom": "Jean", "age": 30, "ville": "Paris"}
+	ligne[1] = ligne[1].split(':')[1].strip().strip('\n')
+	ligne[2] = ligne[2].split(':')[1].strip().strip('\n')
+	print(ligne[1])
+	document = {"question": ligne[0], "xp": int(ligne[1]), "response": bool(ligne[2])}
 	collection.insert_one(document)
 		
 
-	
+client = MongoClient("mongodb://root:rootpassword@localhost:27017/nuitInfo?authSource=admin")
+fd = open("questions.txt", mode = 'r', encoding = 'utf-8', errors='ignore')
+db = client["nuitInfo"]
+collection = db["Quizz"]
+
+while (read3line(fd, collection) != False):
+	pass
 
 	

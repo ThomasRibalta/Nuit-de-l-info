64cd6c8efd982da63099f7f261d4b15ba068c502
Jordan <guaglio.jordan@gmail.com>
2024-12-06 04:37:46 +0100

    add achv

--- nestjs-backend/src/quizz/quizz.controller.ts
+++ nestjs-backend/src/quizz/quizz.controller.ts
@@ -1,13 +1,15 @@
 import { QuizzService } from './quizz.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../jwt/jwt-auth.guard';
 
 @Controller('quizz')
 export class QuizzController {
   constructor(private readonly quizzservice: QuizzService) {}
 
+	@UseGuards(JwtAuthGuard)
   @Get(':id')
-  getQuestion(@Param('id') id: string) {
-    return this.quizzservice.getQuizz(id);
+  getQuestion(@Param('id') id: string, @Req() req: any,) {
+    return this.quizzservice.getQuizz(id, req);
   }
   @Post(':id')
   submitResponse(@Body() response: boolean, @Param('id') id: string) {
--- nestjs-backend/src/quizz/quizz.service.ts
+++ nestjs-backend/src/quizz/quizz.service.ts
@@ -1,3 +1,4 @@
+import { User } from './../users/schema/user.schema';
 import { QuizzSchema, Quizz } from './schema/quizz.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
@@ -5,7 +6,7 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class QuizzService {
-  constructor(@InjectModel('Quizz') private readonly QuizzModel: Model<Quizz>) {}
+  constructor(@InjectModel('Quizz') private readonly QuizzModel: Model<Quizz>, @InjectModel('User') private readonly userModel: Model<User>) {}
 
   async submitResponse(response_user: boolean, id: string) {
     const rep = await this.QuizzModel.findOne({ id: id });
@@ -14,7 +15,13 @@ export class QuizzService {
     else return new HttpException({ rep: rep, result : false}, HttpStatus.OK);
   }
 
-	async getQuizz(id: string) {
+	async getQuizz(id: string, req: any) {
+		if (parseInt(id) > 10)
+		{
+			const user = await this.userModel.findOne({ id:req.user.id });
+			user.achievements = [...user.achievements, "end quizz"]
+			this.userModel.findByIdAndUpdate(id, user);
+		}
     const rep = await this.QuizzModel.findOne({ id: id });
 		if (!rep) return	false
     return new HttpException({ rep: rep }, HttpStatus.OK);

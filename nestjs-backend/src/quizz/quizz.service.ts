4728b0ff20734756ba48fc617650fa9438d28961
Jordan <guaglio.jordan@gmail.com>
2024-12-06 03:55:33 +0100

    end quizz

--- nestjs-backend/src/quizz/quizz.service.ts
+++ nestjs-backend/src/quizz/quizz.service.ts
@@ -5,23 +5,18 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class QuizzService {
-  constructor(
-    @InjectModel('Quizz') private readonly QuizzModel: Model<Quizz>,
-  ) {}
+  constructor(@InjectModel('Quizz') private readonly QuizzModel: Model<Quizz>) {}
 
   async submitResponse(response_user: boolean, id: string) {
     const rep = await this.QuizzModel.findOne({ id: id });
-    if (rep.response == response_user) return true;
-    else return false;
+		if (!rep) return false;
+    if (rep.response == response_user) return new HttpException({ rep: {...rep, result : true}}, HttpStatus.OK);
+    else return new HttpException({ rep: rep, result : false}, HttpStatus.OK);
   }
 
-  async getQuizz(id: string) {
-<<<<<<< HEAD
-    return await this.QuizzModel.findOne({ id: id }).select('-response');
-=======
+	async getQuizz(id: string) {
     const rep = await this.QuizzModel.findOne({ id: id });
-
+		if (!rep) return	false
     return new HttpException({ rep: rep }, HttpStatus.OK);
->>>>>>> b83cc9afc5f46832d207f088a4557d4fad019068
   }
 }
--- nuit-de-linfo-front/src/Quizz/Quizz.jsx
+++ nuit-de-linfo-front/src/Quizz/Quizz.jsx
@@ -1,37 +1,46 @@
 import { useNavigate, useParams, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Quizz() {
-  //const [count, setCount] = useState(0)
   const navigate = useNavigate();
-  const { number } = useParams();
+  let { number } = useParams();
   const root = useLocation();
+	const [data, setData] = useState({});
 
-	useEffect(()=>{ fetch('http://localhost:3030/quizz/1', {
+	useEffect(()=>{ fetch('http://localhost:3030/quizz/' + number, {
 		method: 'GET', // ou 'POST', 'PUT', 'DELETE', etc.
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	})
-	.then(response => response)
-	.then(data => console.log(data));},[])
-
+	.then(response => response.json())
+	.then((data) => {if (data == false) navigate('/Quizz/') ; else setData(data.response.rep)})},[number])
 	const Send = function(response){
-		fetch('http://localhost:3030/quizz/1', {
-			method: 'GET',
+		fetch('http://localhost:3030/quizz/' + number, {		
+			method: 'POST',
+			body : JSON.stringify({response : response}),
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		})
-		.then(response => response)
-		.then(data => data);}
-  return (
+		.then(response => response.json())
+		.then((data) => {if (data == false) navigate('/Quizz/'); 
+			else if (data.response.result == true)
+				console.log("good rep");
+			else
+				console.log("Faux")
+			if (data.response.result == false || data.response.res.response == false)
+				console.log(data.response.rep.explication);})
+		console.log("")
+		number++;
+		navigate('/Quizz/' + (parseInt(number)))}
+  return (	
 		<>
 		<div>
 		<div>
-			<h1>{}</h1>
-			<button onClick={Send("True")}>Vrai</button>
-			<button onClick={Send("False")}>Faux</button>
+			<h1>{data.question}</h1>
+			<button onClick={() => Send("True")}>Vrai</button>
+			<button onClick={() => Send("False")}>Faux</button>
 
 		</div>
 		</div>

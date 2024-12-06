6de6949bb3e2c384383406cef2c3ebbddaa580d2
ThomasRibalta <thomasribalta@gmail.com>
2024-12-06 17:35:58 +0000

    prod

--- nuit-de-linfo-front/vite.config.js
+++ nuit-de-linfo-front/vite.config.js
@@ -6,20 +6,3 @@ export default defineConfig({
   plugins: [react()],
 });
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: '0.0.0.0', // Écoute sur toutes les interfaces
-//     port: 3000, // Définit le port du serveur
-//     hmr: {
-//       host: '148.113.45.177', // Nom d’hôte pour la connexion HMR
-//       clientPort: 3000,  // Port utilisé par le client HMR
-//     },
-//     watch: {
-//       usePolling: true, // Utilise le polling pour la détection des changements
-//     },
-//   },
-// });
